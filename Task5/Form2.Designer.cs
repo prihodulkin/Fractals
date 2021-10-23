bd1c37628cbc9d14c66698219397542bb1432903
nsilantev91 <45269218+nsilantev91@users.noreply.github.com>
2021-10-23 21:20:15 +0300

    Working

--- Task5/Form2.Designer.cs
+++ Task5/Form2.Designer.cs
@@ -29,12 +29,101 @@ namespace Task5
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.roughness_box = new System.Windows.Forms.TextBox();
+            this.plus_btn = new System.Windows.Forms.Button();
+            this.minus_btn = new System.Windows.Forms.Button();
+            this.next_step_btn = new System.Windows.Forms.Button();
+            this.clear_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(8, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Шероховатость";
+            // 
+            // roughness_box
+            // 
+            this.roughness_box.Location = new System.Drawing.Point(12, 57);
+            this.roughness_box.Name = "roughness_box";
+            this.roughness_box.Size = new System.Drawing.Size(69, 22);
+            this.roughness_box.TabIndex = 2;
+            this.roughness_box.TextChanged += new System.EventHandler(this.roughness_box_TextChanged);
+            // 
+            // plus_btn
+            // 
+            this.plus_btn.Location = new System.Drawing.Point(115, 57);
+            this.plus_btn.Name = "plus_btn";
+            this.plus_btn.Size = new System.Drawing.Size(22, 22);
+            this.plus_btn.TabIndex = 3;
+            this.plus_btn.Text = "+";
+            this.plus_btn.UseVisualStyleBackColor = true;
+            this.plus_btn.Click += new System.EventHandler(this.plus_btn_Click);
+            // 
+            // minus_btn
+            // 
+            this.minus_btn.Location = new System.Drawing.Point(87, 57);
+            this.minus_btn.Name = "minus_btn";
+            this.minus_btn.Size = new System.Drawing.Size(22, 22);
+            this.minus_btn.TabIndex = 4;
+            this.minus_btn.Text = "-";
+            this.minus_btn.UseVisualStyleBackColor = true;
+            this.minus_btn.Click += new System.EventHandler(this.minus_btn_Click);
+            // 
+            // next_step_btn
+            // 
+            this.next_step_btn.Enabled = false;
+            this.next_step_btn.Location = new System.Drawing.Point(12, 98);
+            this.next_step_btn.Name = "next_step_btn";
+            this.next_step_btn.Size = new System.Drawing.Size(125, 27);
+            this.next_step_btn.TabIndex = 5;
+            this.next_step_btn.Text = "Следующий шаг";
+            this.next_step_btn.UseVisualStyleBackColor = true;
+            this.next_step_btn.Click += new System.EventHandler(this.next_step_btn_Click);
+            // 
+            // clear_btn
+            // 
+            this.clear_btn.Location = new System.Drawing.Point(12, 145);
+            this.clear_btn.Name = "clear_btn";
+            this.clear_btn.Size = new System.Drawing.Size(125, 27);
+            this.clear_btn.TabIndex = 6;
+            this.clear_btn.Text = "Очистить";
+            this.clear_btn.UseVisualStyleBackColor = true;
+            this.clear_btn.Click += new System.EventHandler(this.clear_btn_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1368, 645);
+            this.Controls.Add(this.clear_btn);
+            this.Controls.Add(this.next_step_btn);
+            this.Controls.Add(this.minus_btn);
+            this.Controls.Add(this.plus_btn);
+            this.Controls.Add(this.roughness_box);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.MouseClick += Form2_MouseClick;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
+
+
+
         #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox roughness_box;
+        private System.Windows.Forms.Button plus_btn;
+        private System.Windows.Forms.Button minus_btn;
+        private System.Windows.Forms.Button next_step_btn;
+        private System.Windows.Forms.Button clear_btn;
     }
 }
--- Task5/Form2.cs
+++ Task5/Form2.cs
@@ -10,11 +10,133 @@ using System.Windows.Forms;
 
 namespace Task5
 {
+    
     public partial class Form2 : Form
     {
+        private Graphics g;
+        List<Segment> segments;
+        double R;
+        Point startPoint1 = new Point(0,0);
+        Point startPoint2 = new Point(0,0);
+
         public Form2()
         {
+            segments = new List<Segment>();
+            R = 1.0;
             InitializeComponent();
+            roughness_box.Text = R.ToString();
+            g = CreateGraphics();
+        }
+
+       
+        private void next_step_btn_Click(object sender, EventArgs e)
+        {
+            next_step_btn.Enabled = false;
+            List<Segment> worked = new List<Segment>();
+            var random = new Random();
+            foreach(var s in segments)
+            {
+                var height = (s.begin.Y + s.end.Y) / 2 + (random.NextDouble() - 0.5) * R * s.GetLength();
+                var centerX = (s.begin.X + s.end.X) / 2;
+                var centerPoint = new Point(centerX, (int)height);
+                worked.Add(new Segment(s.begin, centerPoint));
+                worked.Add(new Segment(centerPoint, s.end));
+            }
+            segments = worked;
+            g.Clear(BackColor);
+            foreach (var s in segments)
+            {
+                g.DrawLine(Pens.Black, s.begin, s.end);
+            }
+            next_step_btn.Enabled = true;
+        }
+
+        private void clear_btn_Click(object sender, EventArgs e)
+        {
+            startPoint1 = new Point(0, 0);
+            startPoint2 = new Point(0, 0);
+            segments.Clear();
+            g.Clear(BackColor);
+            next_step_btn.Enabled = false;
+        }
+
+        private void minus_btn_Click(object sender, EventArgs e)
+        {
+            if (Math.Abs(R - 4) <= 0.0001)
+            {
+                plus_btn.Enabled = true;
+            }
+            R -= 0.1;
+            roughness_box.Text = R.ToString();
+            if (Math.Abs(R - 0.1) <= 0.0001)
+            {
+                minus_btn.Enabled = false;
+                return;
+            }
+        }
+
+        private void plus_btn_Click(object sender, EventArgs e)
+        {
+            if (Math.Abs(R - 0.1) <= 0.0001)
+            {
+                minus_btn.Enabled = true;
+            }
+            R += 0.1;
+            roughness_box.Text = R.ToString();
+            if (Math.Abs(R - 4) <= 0.0001)
+            {
+                plus_btn.Enabled = false;
+                return;
+            }
+        }
+
+        private void Form2_MouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (startPoint1.X == 0)
+            {
+                startPoint1 = new Point(e.X, e.Y);
+                return;
+            }
+
+            if(startPoint2.X == 0)
+            {
+                startPoint2 = new Point(e.X, e.Y);
+                g.DrawLine(Pens.Black, startPoint1.X, startPoint1.Y, startPoint2.X, startPoint2.Y);
+                next_step_btn.Enabled = true;
+                segments.Add(new Segment(startPoint1, startPoint2));
+            }
+            
+        }
+
+        private void roughness_box_TextChanged(object sender, EventArgs e)
+        {
+            var value = roughness_box.Text;
+            double val;
+            if (double.TryParse(value, out val))
+            {
+                if(val > 4 || val < 0.1)
+                {
+                    MessageBox.Show("Значение должно быть от 0,1 до 4");
+                    return;
+                }
+                R = val;
+            }
+
+        }
+    }
+
+    public class Segment
+    {
+        public Point begin;
+        public Point end;
+        public Segment(Point beg, Point end)
+        {
+            begin = beg;
+            this.end = end;
+        }
+        public double GetLength()
+        {
+            return Math.Sqrt(Math.Pow((end.X - begin.X), 2) + Math.Pow((end.Y - begin.Y), 2));
         }
     }
 }
