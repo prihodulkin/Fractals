a764a34f45ca2fb6974f16a725583a6a7d0c7b27
Dunkash <45978367+Dunkash@users.noreply.github.com>
2021-10-26 23:39:57 +0300

    Task Completed

--- Task5/Form3.Designer.cs
+++ Task5/Form3.Designer.cs
@@ -29,12 +29,99 @@ namespace Task5
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.Add = new System.Windows.Forms.ToolStripButton();
+            this.Delete = new System.Windows.Forms.ToolStripButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Add,
+            this.Delete});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 27);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // Add
+            // 
+            this.Add.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.Add.Image = ((System.Drawing.Image)(resources.GetObject("Add.Image")));
+            this.Add.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(80, 24);
+            this.Add.Text = "Add point";
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.Delete.Image = ((System.Drawing.Image)(resources.GetObject("Delete.Image")));
+            this.Delete.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(57, 24);
+            this.Delete.Text = "Delete";
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 423);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.trackBar1.Location = new System.Drawing.Point(0, 425);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(800, 25);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Visible = false;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form3";
             this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton Add;
+        private System.Windows.Forms.ToolStripButton Delete;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
     }
 }
--- Task5/Form3.cs
+++ Task5/Form3.cs
@@ -12,9 +12,152 @@ namespace Task5
 {
     public partial class Form3 : Form
     {
+        int mode = 0;
+        List<Button> points = new List<Button>();
+        float t = 0;
         public Form3()
         {
             InitializeComponent();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            t = ((float)trackBar1.Value) / 100;
+            pictureBox1.Refresh();
+        }
+
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+        }
+
+        private PointF getPoint(List<PointF> points)
+        {
+            if (t == 0)
+                return (points[0]);
+            var res = new PointF();
+            for (int i = 0; i < points.Count; i++)
+            {
+                var l = bernstein(points.Count - 1, i, t);
+                res.X += points[i].X * l;
+                res.Y += points[i].Y * l;
+            }
+            return res;
+        }
+
+        private PointF getPoint(List<PointF> points, float T)
+        {
+            if (T == 0)
+                return (points[0]);
+            var res = new PointF(0,0);
+            for (int i = 0; i < points.Count; i++)
+            {
+                var l = bernstein(points.Count - 1, i, T);
+                res.X += points[i].X * l;
+                res.Y += points[i].Y * l;
+            }
+            return res;
+        }
+
+        private List<PointF>GetPoints(float interval=0.01f)
+        {
+            var pts = new List<PointF>();
+            foreach (var i in points)
+            {
+                pts.Add(i.Location);
+            }
+            List<PointF> res = new List<PointF>();
+            for (float tt = 0;tt<=1;tt+=interval)
+            {
+                res.Add(getPoint(pts, tt));
+            }
+            return res;
+        }
+
+
+        private float fact(int i)
+        {
+            float res = 1;
+            for (float j = 1; j <= i; j++)
+                res *= j;
+            return res;
+        }
+
+        float binominal(int n, int i)
+        {
+            return fact(n) / (fact(i) * fact(n-i));
+        }
+
+        float bernstein(int n, int i, float T)
+        {
+            float a = (float)Math.Pow(T, i);
+            float b = (float)Math.Pow((1 - T), (n - i));
+            return binominal(n, i) * a * b;
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            mode = 0;
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            mode = 1;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (mode == 0)
+            {
+                Button but = new Button();
+                but.FlatStyle = FlatStyle.Flat;
+                but.Size = new Size(6, 6);
+
+                but.BackColor = Color.Green;
+                but.Click += Deleter;
+                but.MouseMove += Ref;
+                pictureBox1.Controls.Add(but);
+                but.Location = new Point(e.X-3, e.Y-3);
+                ControlExtension.Draggable(but, true);
+                points.Add(but);
+            }
+            pictureBox1.Refresh();
+        }
+
+
+        private void Deleter(object sender, EventArgs e)
+        {
+            if (mode == 1)
+            {
+                pictureBox1.Controls.Remove((Button)sender);
+                points.Remove((Button)sender);
+            }
+            pictureBox1.Refresh();
+        }
+
+        private void Ref(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            /*
+            if (points.Count > 0)
+            {
+                var a = new SolidBrush(Color.Red);
+                e.Graphics.FillRectangle(a, getPoint(points).X, getPoint(points).Y, 5, 5);
+            }
+            */
+            if (points.Count > 0)
+            {
+                var a = new SolidBrush(Color.Red);
+                var b = GetPoints((float)1/(points.Count*500));
+                foreach (var i in b)
+                {
+                    e.Graphics.FillRectangle(a, i.X, i.Y, 2, 2);
+                }
+            }
         }
     }
 }
